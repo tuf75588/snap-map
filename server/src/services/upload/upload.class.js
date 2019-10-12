208e32f74fe803f6f75b03daa148848f3abd15c9
Andrew Davis <atd285@gmail.com>
2019-10-11 20:14:58 -0400

    add new service for image backend uploads

---
+++ server/src/services/upload/upload.class.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+
+const fetch = require('node-fetch');
+exports.Upload = class Upload {
+  constructor(options, app) {
+    this.options = options || {};
+    this.app = app;
+  }
+
+  async create(data) {
+    const headers = {
+      Authorization: `Client-ID ${this.app.get('imgur')}`,
+      'Content-Type': 'application/json'
+    };
+    const response = await fetch('https://api.imgur.com/3/upload', {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({
+        image: data.image,
+        type: 'base64'
+      })
+    });
+    const json = await response.json();
+    return json;
+  }
+};
